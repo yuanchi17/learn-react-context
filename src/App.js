bf64336d50440993d8386de5cfa564b69f9626a6
李元琦 <yuan.chi@program.com.tw>
2022-08-04 14:40:00 +0800

    feat: 新增 router

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom'
+import Home from './pages/Home'
+import React from 'react'
+import TodoList from './pages/TodoList'
 
-function App() {
+function App () {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <BrowserRouter>
+      <ul>
+        <li><Link to='/'>Home</Link></li>
+        <li><Link to='/todo-list'>Todo List</Link></li>
+      </ul>
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/todo-list" element={<TodoList />}></Route>
+      </Routes>
+    </BrowserRouter>
+  )
 }
 
-export default App;
+export default App
---
+++ src/pages/Home.js
@@ -0,0 +1,11 @@
+import React, { } from 'react'
+
+const Home = () => {
+  return (
+    <>
+      Hi！
+    </>
+  )
+}
+
+export default Home
